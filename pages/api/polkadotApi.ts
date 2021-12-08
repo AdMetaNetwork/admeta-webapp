43ad850c6a51f671b2e5c1511e19ee27b817fecb
Han <4738254+h4n0@users.noreply.github.com>
2021-12-08 16:53:09 +0100

    Fix string decoding issue

--- pages/api/polkadotApi.ts
+++ pages/api/polkadotApi.ts
@@ -1,24 +1,23 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
-
+import { ApiPromise, WsProvider } from "@polkadot/api";
 
 export class PolkadotChainConnector {
-    async getTargetMetadata(): Promise<any> {
+  async getTargetMetadata(): Promise<any> {
+    const wsProvider = new WsProvider("ws://testnet.admeta.network:9944");
+    const api = await ApiPromise.create({ provider: wsProvider });
 
-        const wsProvider = new WsProvider('ws://testnet.admeta.network:9944');
-        const api = await ApiPromise.create({ provider: wsProvider });
+    const resp = await api.query.ad.impressionAds(1);
 
-        const resp = await api.query.ad.impressionAds(1);
-        // let respJson = resp.toJSON();
-        // console.log('getTargetMetadata:' + JSON.stringify(respJson));
-        let respStr = JSON.parse(resp.toString());
-        // let meta = metadataJson?['metadata']
-        // metadataJson["metadata"]
-        let meta = respStr['metadata'];
-        // let parsedMeta = String.fromCharCode(meta);
-        // let parsedMeta =meta.toString();
-        let parsedMeta = Buffer.from(meta.toString(), "hex").toString();
-        console.log('meta is: ' + JSON.stringify(meta));
-        console.log("parsed meta is: " + parsedMeta);
-        return meta;
-    }
+    // let respJson = resp.toJSON();
+    // console.log('getTargetMetadata:' + JSON.stringify(respJson));
+    let respStr = JSON.parse(resp.toString());
+    // let meta = metadataJson?['metadata']
+    // metadataJson["metadata"]
+    let meta = respStr["metadata"];
+    // let parsedMeta = String.fromCharCode(meta);
+    // let parsedMeta =meta.toString();
+    let parsedMeta = Buffer.from(meta.slice(2), "hex").toString();
+    console.log("meta is: " + JSON.stringify(meta));
+    console.log("parsed meta is: " + parsedMeta);
+    return meta;
+  }
 }
